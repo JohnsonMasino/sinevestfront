396e9c8ea64ea01b492f4f79505b12fecb02ac9d
Johnson Masino <johnsonmasino@gmail.com>
2026-08-12 11:56:22 +0100

    Added the header and footer

---
+++ sinevestfront/src/pages/outer/HomePage.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/outside/Header";
+import Footer from "../../components/outside/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+        <Header />
+        <Footer />
+    </>
+  );
+};
+
+export default HomePage;
